c5c185e36d333f6aca2be29a4f969569d98fcac1
Travis Kern <kern.travis@gmail.com>
2023-02-05 20:30:30 -0500

    Update container run scripts for name specification
    Added -n flag to container run scripts to allow an additional string to
    be added to the container name.
    
    For example, the default container name is "omnicron". But adding "-n
    special" will set the container name to "omnicron-special".

--- scripts/container-setup.sh
+++ scripts/container-setup.sh
@@ -3,22 +3,36 @@ project_dir=$(cd .. && pwd)
 docker_project="/app/omnicron"
 image_name="omnicron"
 container_name="omnicron"
+container_name_extra=""
 
 usage () {
-    echo "Usage: $script_name -c <config-filename>"
+    echo "Usage: $script_name -c <config-filename> [-n container-name-extra]"
+    echo ""
+    echo "    config-filename      : Config filename to use. (Required)"
+    echo "    container-name-extra : Additional string to add to container name. (Optional)"
     exit 1
 }
 
-while getopts c: flag
+while getopts ":c:n:" flag
 do
     case "${flag}" in
-        c) config_filename=${OPTARG};;
+        c) config_filename=${OPTARG}
+           ;;
+        n) container_name_extra=${OPTARG}
+           ;;
+        *)
+            usage
     esac
 done
 
-if [ -z $config_filename ]; then
+if [ -z "$config_filename" ]; then
     echo "Missing required argument: <config-filename>"
+    echo ""
     usage
+fi
+
+if [ -n "$container_name_extra" ]; then
+    container_name="$container_name-$container_name_extra"
 fi
 
 config_dir="$project_dir/config"
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 source ./container-setup.sh
 
+usage () {
+    echo "Usage: $script_name -c <config-filename>"
+    exit 1
+}
+
+while getopts ":c:n:" flag
+do
+    case "${flag}" in
+        c) config_filename=${OPTARG}
+           ;;
+        n) container_name_extra=${OPTARG}
+           ;;
+        *)
+            usage
+    esac
+done
+
 docker run -d -v $config_dir:$docker_project/config \
     -v $log_dir:$docker_project/logs \
     -v $data_dir:$docker_project/data \
