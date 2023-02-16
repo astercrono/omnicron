8ac8cce79befd3454aa626efd7d5b784dd90a713
Travis Kern <kern.travis@gmail.com>
2023-02-16 10:22:04 -0500

    Update docker run script
    Run app via bundle exec.
    
    This is necessary to pick up the discordrb
    gem that is now being pulled in via Git.

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -22,4 +22,4 @@ docker run -d -v $config_dir:$docker_project/config \
     -v $log_dir:$docker_project/logs \
     -v $data_dir:$docker_project/data \
     --name $container_name $image_name \
-    /bin/bash -c "ruby run.rb --config config/$config_filename"
+    /bin/bash -c "bundle exec run.rb --config config/$config_filename"
